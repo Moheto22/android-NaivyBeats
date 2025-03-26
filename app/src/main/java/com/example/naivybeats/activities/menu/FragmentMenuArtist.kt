7a9042728bf9b913640824f513b4e8ec679d6d58
moheto22 <mohatae61@gmail.com>
2025-03-26 18:55:44 +0100

    Creado el buscador

--- app/src/main/java/com/example/naivybeats/activities/menu/FragmentMenuArtist.kt
+++ app/src/main/java/com/example/naivybeats/activities/menu/FragmentMenuArtist.kt
@@ -27,7 +27,7 @@ class FragmentMenuArtist : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
                              ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_artist, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
     companion object {
--- app/src/main/java/com/example/naivybeats/activities/menu/FragmentMenuSpace.kt
+++ app/src/main/java/com/example/naivybeats/activities/menu/FragmentMenuSpace.kt
@@ -34,7 +34,7 @@ class FragmentMenuSpace : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
                              ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_menu_space, container, false)
+        return inflater.inflate(R.layout.fragment_menu, container, false)
     }
 
     companion object {
