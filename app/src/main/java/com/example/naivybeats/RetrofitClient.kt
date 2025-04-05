6dc0a8e2aa87bfde8dd0d2861a398dfbbe7bcd6f
Brunicoo <brunoconvaliarejas7@gmail.com>
2025-04-05 21:14:44 +0200

    local changes

--- app/src/main/java/com/example/naivybeats/RetrofitClient.kt
+++ app/src/main/java/com/example/naivybeats/RetrofitClient.kt
@@ -16,7 +16,7 @@ import java.time.format.DateTimeFormatter
 object RetrofitClient {
     //private const val BASE_URL = "http://10.0.2.210/NaivyBeats/"
     //private const val BASE_URL = "http://10.0.0.99/dam04/"
-    private const val BASE_URL = "http://192.168.56.1/NaivyBeats/"
+    private const val BASE_URL = "http://192.168.1.28/NaivyBeats/"
 
     private val retrofit: Retrofit by lazy {
         val gson = GsonBuilder()
